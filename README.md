df943a6d25b94642d57473f2f53856766cc73ade
WafaMili <miliwafa94@gmail.com>
2025-01-12 20:57:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Test_Vayetek
+# Calcul de la Somme des Valeurs d'Étalonnage
+
+Ce projet contient un script Node.js qui calcule la somme des valeurs d'étalonnage en extrayant les chiffres d'un fichier texte. L'algorithme identifie le premier et le dernier chiffre de chaque ligne, les combine, et les additionne pour obtenir une somme totale.
+
+## Description
+
+Le script parcourt chaque ligne du fichier Document.txt, extrait les chiffres présents, et calcule une valeur pour chaque ligne basée sur la concaténation du premier et du dernier chiffre. Ces valeurs sont ensuite additionnées pour produire un résultat final.
+
+# Résultat 
+La somme totale des valeurs d'étalonnage est : 33740
---
+++ Test_Technique.js
@@ -0,0 +1,33 @@
+function calculerSommedétalonnage(lignes) {
+  let somme = 0;
+
+  lignes.forEach(ligne => {
+      let chiffres = [];
+      
+      for (let i = 0; i < ligne.length; i++) {
+          const char = ligne[i];
+          if (char >= '0' && char <= '9') {
+              chiffres.push(char);
+          }
+      }
+      if (chiffres.length >= 2) {
+          const premier = chiffres[0];
+          const dernier = chiffres[chiffres.length - 1];
+          const valeur = parseInt(premier + dernier, 10);
+          somme += valeur;
+      }
+  });
+
+  return somme;
+}
+
+const fs = require('fs');
+fs.readFile('Document.txt', 'utf8', (err, data) => {
+  if (err) {
+      console.error("Erreur lors de la lecture du fichier :", err);
+      return;
+  }
+  const lignes = data.split('\n');
+  const sommeTotale = calculerSommeValeursSansMatch(lignes);
+  console.log("La somme totale des valeurs d'étalonnage est :", sommeTotale);
+});
